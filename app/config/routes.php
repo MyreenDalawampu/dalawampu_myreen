9fe3b38be60402e5827ec4fe2fe23a8dab4c37fd
MyreenDalawampu <myreendalawampu194@gmail.com>
2025-09-13 06:51:35 +0800

    update

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 
-$router->get('/students', 'StudentsController::index'); 
+$router->get('/', 'StudentsController::index'); 
 $router->match('/students/create', 'StudentsController::create', ['GET', 'POST']);
 $router->match('/students/update/{id}', 'StudentsController::update', ['GET', 'POST']);
 $router->get('/students/delete/{id}', 'StudentsController::delete');
