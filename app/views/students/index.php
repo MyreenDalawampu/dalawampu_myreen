d82a933986caaaf76db80b21463aa926b3faae05
MyreenDalawampu <myreendalawampu194@gmail.com>
2025-09-19 21:20:59 +0800

    try

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -8,7 +8,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students List</title>
-    <link rel="stylesheet" href="<?=base_url();?>/public/style.css">
+    <link rel="stylesheet" href="<?=base_url();?>public/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
